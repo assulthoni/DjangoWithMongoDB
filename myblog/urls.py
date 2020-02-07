6e3f061378bcbe671eaad3a469a324e40f645c5d
Ahmad Shohibus Sulthoni <thoni.shohibus@gmail.com>
2020-02-07 18:45:42 +0700

    add new page

--- myblog/urls.py
+++ myblog/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('projects/', views.projects)
 ]
